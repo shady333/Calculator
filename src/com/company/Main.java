014ebeedb1ca5705c7da5d76c281cfa1a5a696dd
Oleg <oleg.dudar@gmail.com>
2019-02-14 17:09:11 +0200

    small changes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,16 +7,17 @@ public class Main {
     public static void main(String[] args) {
         // create a scanner so we can read the command-line input
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter some for start");
+        System.out.print("Press any key to start");
         while (!scanner.next().equals("exit")) {
-            System.out.print("Enter operation: add, sub, mult, div ");
+            System.out.print("Choose operation: add, sub, mult, div ");
             String operation = scanner.next();
 
             System.out.print("Enter first number: ");
 
             int first_numb = scanner.nextInt();
+            //TODO extract method which will be used for number 1 and number 2
             while (first_numb > 20 || first_numb < -20) {
-                System.out.print("The number must be in the range -20 to 20. Enter please number: ");
+                System.out.print("The number must be in range from -20 to 20. Please enter correct number: ");
                 first_numb = scanner.nextInt();
             }
 
@@ -45,8 +46,8 @@ public class Main {
                     break;
             }
 
-
-                System.out.println(String.format("Result of operation is %d,  For exit enter 'exit', or something for continue", res));
+            //TODO add operation name into output
+            System.out.println(String.format("Result of operation is %d,  For exit enter 'exit', or something for continue", res));
         }
     }
 }
